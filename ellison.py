c192af7e3b18d7622b52d1993f9bbce71e405385
Atsushi Yamagishi <haru.su.jp@gmail.com>
2014-08-13 16:36:10 +0900

    defined a class called "player" and two functions. "update" is used to proceed the game. "Randomization" is not yet incorporated

---
+++ ellison.py
@@ -0,0 +1,46 @@
+from __future__ import division
+import numpy as np
+import random
+
+#This program is designed for the 2*2 "coordination" game
+
+
+
+payoff = np.array([[4,0],[3,2]]) # all players share the same payoff matrix
+p = 1/3 #If the proportion of players taking 1 exceeds this,other players are better off to take 1
+N = 10 # the number of players
+n = 1 # the number of neighbors on one side.it is 2 in total when n = 1
+epsilon = 0.1
+actions = [0,1]
+
+class player:
+	def __init__(self,actions):
+		int_action = random.choice(actions) # the initial action is randomly choosed.
+		self.action = int_action
+	
+players = [player(actions) for i in range(N)] #list of all players in the game
+
+def show_action_profile():
+	print [players[i].action for i in range(N)]
+	
+def update():
+	# pick a player which can change the action
+	d = random.choice(range(N))
+	nei_numbers = [] # contains the number assigned to each neighbor
+	nei_numbers_with_large_small = [i for i in range(-n,n+1)] # contains the chosen player,may contain inappropriate numbers
+	del nei_numbers_with_large_small[n] #delete the chosen player
+	for i in nei_numbers_with_large_small:
+		if i < 0:
+			i = i + N
+
+		if i >= N:
+			i = i - N
+
+		nei_numbers.append(i)
+			
+	nei_action = [players[i].action for i in nei_numbers] # neighbors' action profile
+		
+	if sum(nei_action) > p:   # change the action
+		players[d].action = 1
+	else:
+		players[d].action = 0
